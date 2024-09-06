ec8969599f4862676d2333f57018642f4d597c66
Bhavesh Gondane <72034106+bhavesh-gondane@users.noreply.github.com>
2024-09-06 11:22:41 +0530

    Update Leap_year_switch.java

--- OOP's with Java/Assignments/Assignment2/Leap_year_switch.java
+++ OOP's with Java/Assignments/Assignment2/Leap_year_switch.java
@@ -1,3 +1,5 @@
+//QUE: Write a program that checks if a given year is a leap year or not using switch-case.
+
 package assignment1_dt4_9;
 
 import java.util.Scanner;
@@ -5,25 +7,6 @@ import java.util.Scanner;
 public class Leap_year_switch {
 
 	public static void main(String[] args) {
-//		System.out.println("Enter a year");
-//		Scanner sc = new Scanner(System.in);
-//		int yr=sc.nextInt();
-//		if((yr%4==0 && yr%100!=0) || (yr%4==0 && yr%100==0 && yr%400==0)) {
-//			
-//		}
-		
-//int choice = 0;
-		//		int choice = sc.nextInt();
-//		switch(yr) {
-//		case 1:
-//			System.out.println("It is a leap");
-//			break;
-//		//case 2:
-//			//System.out.println("It is a leap");
-//			//break;
-//		default:
-//			System.out.println("It is not a leap year");
-//		}
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("ENter a year");
