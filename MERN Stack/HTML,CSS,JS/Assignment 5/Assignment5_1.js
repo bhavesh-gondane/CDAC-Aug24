81d626d64db1f0637901faa6da680d5cda9f53e1
Bhavesh Gondane <72034106+bhavesh-gondane@users.noreply.github.com>
2024-11-07 11:31:37 +0530

    Add files via upload

---
+++ MERN Stack/HTML,CSS,JS/Assignment 5/Assignment5_1.js
@@ -0,0 +1,22 @@
+/*
+    1. Print out "Program started" at the start of your code
+    2. Create a Promise that resolves after 3 seconds
+    3. Log out the promise while it's pending
+    4. Print out "Program in progress..." as well
+    5. Print out "Program complete" when the promise completes after 3 seconds
+
+    HINT: Use setTimeout for the delay
+*/
+
+console.log("Program started");
+const myPromise = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve();
+    },3000)
+})
+console.log(myPromise);
+console.log("Program in progress...");
+
+myPromise.then(()=>{
+    console.log("Program complete");
+})
